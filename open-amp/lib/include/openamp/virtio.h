7ca13cdcf107aee7e60ca1e685748da199dcd254
Carlo Caione <ccaione@baylibre.com>
2023-07-17 17:44:37 +0200

    Sync with openamp project sha1 5891cb4b2e81
    Synchronize code with sha1 5891cb4b2e813d1c9d10def998952b062304e43f
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- open-amp/lib/include/openamp/virtio.h
+++ open-amp/lib/include/openamp/virtio.h
@@ -8,6 +8,7 @@
 #define _VIRTIO_H_
 
 #include <openamp/virtqueue.h>
+#include <metal/errno.h>
 #include <metal/spinlock.h>
 
 #if defined __cplusplus
@@ -15,23 +16,55 @@ extern "C" {
 #endif
 
 /* VirtIO device IDs. */
-#define VIRTIO_ID_NETWORK    0x01UL
-#define VIRTIO_ID_BLOCK      0x02UL
-#define VIRTIO_ID_CONSOLE    0x03UL
-#define VIRTIO_ID_ENTROPY    0x04UL
-#define VIRTIO_ID_BALLOON    0x05UL
-#define VIRTIO_ID_IOMEMORY   0x06UL
-#define VIRTIO_ID_RPMSG      0x07UL /* remote processor messaging */
-#define VIRTIO_ID_SCSI       0x08UL
-#define VIRTIO_ID_9P         0x09UL
-#define VIRTIO_DEV_ANY_ID    (-1)UL
+#define VIRTIO_ID_NETWORK        1UL
+#define VIRTIO_ID_BLOCK          2UL
+#define VIRTIO_ID_CONSOLE        3UL
+#define VIRTIO_ID_ENTROPY        4UL
+#define VIRTIO_ID_BALLOON        5UL
+#define VIRTIO_ID_IOMEMORY       6UL
+#define VIRTIO_ID_RPMSG          7UL /* remote processor messaging */
+#define VIRTIO_ID_SCSI           8UL
+#define VIRTIO_ID_9P             9UL
+#define VIRTIO_ID_MAC80211_WLAN  10UL
+#define VIRTIO_ID_RPROC_SERIAL   11UL
+#define VIRTIO_ID_CAIF           12UL
+#define VIRTIO_ID_MEMORY_BALLOON 13UL
+#define VIRTIO_ID_GPU            16UL
+#define VIRTIO_ID_CLOCK          17UL
+#define VIRTIO_ID_INPUT          18UL
+#define VIRTIO_ID_VSOCK          19UL
+#define VIRTIO_ID_CRYPTO         20UL
+#define VIRTIO_ID_SIGNAL_DIST    21UL
+#define VIRTIO_ID_PSTORE         22UL
+#define VIRTIO_ID_IOMMU          23UL
+#define VIRTIO_ID_MEM            24UL
+#define VIRTIO_ID_SOUND          25UL
+#define VIRTIO_ID_FS             26UL
+#define VIRTIO_ID_PMEM           27UL
+#define VIRTIO_ID_RPMB           28UL
+#define VIRTIO_ID_MAC80211_HWSIM 29UL
+#define VIRTIO_ID_VIDEO_ENCODER  30UL
+#define VIRTIO_ID_VIDEO_DECODER  31UL
+#define VIRTIO_ID_SCMI           32UL
+#define VIRTIO_ID_NITRO_SEC_MOD  33UL
+#define VIRTIO_ID_I2C_ADAPTER    34UL
+#define VIRTIO_ID_WATCHDOG       35UL
+#define VIRTIO_ID_CAN            36UL
+#define VIRTIO_ID_PARAM_SERV     38UL
+#define VIRTIO_ID_AUDIO_POLICY   39UL
+#define VIRTIO_ID_BT             40UL
+#define VIRTIO_ID_GPIO           41UL
+#define VIRTIO_ID_RDMA           42UL
+#define VIRTIO_DEV_ANY_ID        -1UL
 
 /* Status byte for guest to report progress. */
-#define VIRTIO_CONFIG_STATUS_ACK       0x01
-#define VIRTIO_CONFIG_STATUS_DRIVER    0x02
-#define VIRTIO_CONFIG_STATUS_DRIVER_OK 0x04
+#define VIRTIO_CONFIG_STATUS_RESET       0x00
+#define VIRTIO_CONFIG_STATUS_ACK         0x01
+#define VIRTIO_CONFIG_STATUS_DRIVER      0x02
+#define VIRTIO_CONFIG_STATUS_DRIVER_OK   0x04
+#define VIRTIO_CONFIG_FEATURES_OK        0x08
 #define VIRTIO_CONFIG_STATUS_NEEDS_RESET 0x40
-#define VIRTIO_CONFIG_STATUS_FAILED    0x80
+#define VIRTIO_CONFIG_STATUS_FAILED      0x80
 
 /* Virtio device role */
 #define VIRTIO_DEV_DRIVER	0UL
@@ -144,6 +177,11 @@ void virtio_describe(struct virtio_device *dev, const char *msg,
  */
 
 struct virtio_dispatch {
+	int (*create_virtqueues)(struct virtio_device *vdev,
+				 unsigned int flags,
+				 unsigned int nvqs, const char *names[],
+				 vq_callback callbacks[]);
+	void (*delete_virtqueues)(struct virtio_device *vdev);
 	uint8_t (*get_status)(struct virtio_device *dev);
 	void (*set_status)(struct virtio_device *dev, uint8_t status);
 	uint32_t (*get_features)(struct virtio_device *dev);
@@ -164,9 +202,210 @@ struct virtio_dispatch {
 	void (*notify)(struct virtqueue *vq);
 };
 
+/**
+ * @brief Create the virtio device virtqueue.
+ *
+ * @param vdev		Pointer to virtio device structure.
+ * @param flags		Create flag.
+ * @param nvqs		The virtqueue number.
+ * @param names		Virtqueue names.
+ * @param callbacks	Virtqueue callback functions.
+ *
+ * @return 0 on success, otherwise error code.
+ */
 int virtio_create_virtqueues(struct virtio_device *vdev, unsigned int flags,
 			     unsigned int nvqs, const char *names[],
 			     vq_callback callbacks[]);
+
+/**
+ * @brief Delete the virtio device virtqueue.
+ *
+ * @param vdev	Pointer to virtio device structure.
+ *
+ * @return 0 on success, otherwise error code.
+ */
+static inline int virtio_delete_virtqueues(struct virtio_device *vdev)
+{
+	if (!vdev)
+		return -EINVAL;
+
+	if (!vdev->func || !vdev->func->delete_virtqueues)
+		return -ENXIO;
+
+	vdev->func->delete_virtqueues(vdev);
+	return 0;
+}
+
+/**
+ * @brief Retrieve device status.
+ *
+ * @param dev		Pointer to device structure.
+ * @param status	Pointer to the virtio device status.
+ *
+ * @return 0 on success, otherwise error code.
+ */
+static inline int virtio_get_status(struct virtio_device *vdev, uint8_t *status)
+{
+	if (!vdev || !status)
+		return -EINVAL;
+
+	if (!vdev->func || !vdev->func->get_status)
+		return -ENXIO;
+
+	*status = vdev->func->get_status(vdev);
+	return 0;
+}
+
+/**
+ * @brief Set device status.
+ *
+ * @param dev		Pointer to device structure.
+ * @param status	Value to be set as device status.
+ *
+ * @return 0 on success, otherwise error code.
+ */
+static inline int virtio_set_status(struct virtio_device *vdev, uint8_t status)
+{
+	if (!vdev)
+		return -EINVAL;
+
+	if (!vdev->func || !vdev->func->set_status)
+		return -ENXIO;
+
+	vdev->func->set_status(vdev, status);
+	return 0;
+}
+
+/**
+ * @brief Retrieve configuration data from the device.
+ *
+ * @param dev		Pointer to device structure.
+ * @param offset	Offset of the data within the configuration area.
+ * @param dst		Address of the buffer that will hold the data.
+ * @param len		Length of the data to be retrieved.
+ *
+ * @return 0 on success, otherwise error code.
+ */
+static inline int virtio_read_config(struct virtio_device *vdev,
+				     uint32_t offset, void *dst, int len)
+{
+	if (!vdev || !dst)
+		return -EINVAL;
+
+	if (!vdev->func || !vdev->func->read_config)
+		return -ENXIO;
+
+	vdev->func->read_config(vdev, offset, dst, len);
+	return 0;
+}
+
+/**
+ * @brief Write configuration data to the device.
+ *
+ * @param dev		Pointer to device structure.
+ * @param offset	Offset of the data within the configuration area.
+ * @param src		Address of the buffer that holds the data to write.
+ * @param len		Length of the data to be written.
+ *
+ * @return 0 on success, otherwise error code.
+ */
+static inline int virtio_write_config(struct virtio_device *vdev,
+				      uint32_t offset, void *src, int len)
+{
+	if (!vdev || !src)
+		return -EINVAL;
+
+	if (!vdev->func || !vdev->func->write_config)
+		return -ENXIO;
+
+	vdev->func->write_config(vdev, offset, src, len);
+	return 0;
+}
+
+/**
+ * @brief Get the virtio device features.
+ *
+ * @param dev		Pointer to device structure.
+ * @param features	Pointer to features supported by both the driver and
+ *			the device as a bitfield.
+ *
+ * @return 0 on success, otherwise error code.
+ */
+static inline int virtio_get_features(struct virtio_device *vdev,
+				      uint32_t *features)
+{
+	if (!vdev || !features)
+		return -EINVAL;
+
+	if (!vdev->func || !vdev->func->get_features)
+		return -ENXIO;
+
+	*features = vdev->func->get_features(vdev);
+	return 0;
+}
+
+/**
+ * @brief Set features supported by the VIRTIO driver.
+ *
+ * @param dev		Pointer to device structure.
+ * @param features	Features supported by the driver as a bitfield.
+ *
+ * @return 0 on success, otherwise error code.
+ */
+static inline int virtio_set_features(struct virtio_device *vdev,
+				      uint32_t features)
+{
+	if (!vdev)
+		return -EINVAL;
+
+	if (!vdev->func || !vdev->func->set_features)
+		return -ENXIO;
+
+	vdev->func->set_features(vdev, features);
+	return 0;
+}
+
+/**
+ * @brief Negotiate features between virtio device and driver.
+ *
+ * @param dev			Pointer to device structure.
+ * @param features		Supported features.
+ * @param final_features	Pointer to the final features after negotiate.
+ *
+ * @return 0 on success, otherwise error code.
+ */
+static inline int virtio_negotiate_features(struct virtio_device *vdev,
+					    uint32_t features,
+					    uint32_t *final_features)
+{
+	if (!vdev || !final_features)
+		return -EINVAL;
+
+	if (!vdev->func || !vdev->func->negotiate_features)
+		return -ENXIO;
+
+	*final_features = vdev->func->negotiate_features(vdev, features);
+	return 0;
+}
+
+/**
+ * @brief Reset virtio device.
+ *
+ * @param vdev	Pointer to virtio_device structure.
+ *
+ * @return 0 on success, otherwise error code.
+ */
+static inline int virtio_reset_device(struct virtio_device *vdev)
+{
+	if (!vdev)
+		return -EINVAL;
+
+	if (!vdev->func || !vdev->func->reset_device)
+		return -ENXIO;
+
+	vdev->func->reset_device(vdev);
+	return 0;
+}
 
 #if defined __cplusplus
 }
