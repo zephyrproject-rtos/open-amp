9cd3e30d0665de23ee9ac552835de1cfe13d2001
Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
2024-09-30 10:47:15 +0200

    lib: utils: implement internal strlcpy function
    The strlcpy() function has only recently become available in glibc.
    To ensure compatibility with legacy libc versions, this commit
    implements an internal version of strlcpy().
    
    The function has been adapted from the FreeBSD implementation to
    fit our needs.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
    (cherry picked from commit c7c85bcfeb4f2c1be772efea8a889aa796d1a7c1)
    
    Upstream PR: https://github.com/OpenAMP/open-amp/pull/620
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

---
+++ open-amp/lib/include/internal/string.h
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2024, STMicroelectronics
+ *
+ */
+
+#include <string.h>
+
+size_t strlcpy(char *dest, const char *src, size_t size);
